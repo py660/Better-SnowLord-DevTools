4a667d0808d095325555b7c0bfa540a1cf6c8b67
py660 <93660666+py660@users.noreply.github.com>
2023-03-30 13:55:18 -0400

    Update unminified.js

--- unminified.js
+++ unminified.js
@@ -1,6 +1,6 @@
 (function() {
     var x = document.createElement("script");
     x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";
-    x.onload = alert("Loaded Developer Console!");
+    x.onerror = alert("Error Loading Developer Console!");
     document.head.appendChild(x);
 })()
